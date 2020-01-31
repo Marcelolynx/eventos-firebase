e58cfe6a572083b51e2a9f3ce9e258c5cc77d769
Marcelo Oliveira <marcelolynx@gmail.com>
2020-01-31 17:42:27 -0400

    criando a table para lista de officios

--- src/components/evento-grid/evento-grid.js
+++ src/components/evento-grid/evento-grid.js
@@ -1,10 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './evento-grid.css';
 
 function EventoGrid() {
 
   return(
-    <div>Lista de Ofícios</div>
+    <>
+    <div className="container">
+    <div className="mb-3"><strong>Lista de Ofícios</strong></div>
+
+    <table class="table table-hover table-responsive-md">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Código</th>
+      <th scope="col">Titulo</th>
+      <th scope="col">Requisitante</th>
+      <th scope="col">Data Recebido</th>
+      <th scope="col">Prazo Resposta</th>
+      <th scope="col">Detalhes</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td><a className="btn btn-outline-secondary btn-small">Detalhes</a></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td>@fat</td>
+      <td>@fat</td>
+      <td><a className="btn btn-outline-secondary btn-small">Detalhes</a></td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+      <td>@twitter</td>
+      <td>@twitter</td>
+      <td><a className="btn btn-outline-secondary btn-small">Detalhes</a></td>
+    </tr>
+  </tbody>
+</table>
+</div>
+    </>
   )
 }
 
--- src/view/dashboard/dashboard.js
+++ src/view/dashboard/dashboard.js
@@ -1,27 +1,15 @@
 import React  from 'react';
 import './dashboard.css';  
-import Navbar  from '../../components/navbar/navbar' 
+import Navbar  from '../../components/navbar/navbar';
+import EventoGrid from '../../components/evento-grid/evento-grid';
 
 function Dashboard() {
 
   return(
     <>
     <Navbar/>
-    <div className="container">
-      <div className="card"></div>
-      <h3>Lista de oficios</h3>
-      <div className="form-control">
-        <ul className=""><li>Lista de Officios aqui...</li></ul>
-        <ul className=""><li>Lista de Officios aqui...</li></ul>
-        <ul className=""><li>Lista de Officios aqui...</li></ul>
-        <ul className=""><li>Lista de Officios aqui...</li></ul>
-        <ul className=""><li>Lista de Officios aqui...</li></ul>
-        <ul className=""><li>Lista de Officios aqui...</li></ul>
-        <ul className=""><li>Lista de Officios aqui...</li></ul>
-        <ul className=""><li>Lista de Officios aqui...</li></ul>
-        <ul className=""><li>Lista de Officios aqui...</li></ul>
-      </div>
-    </div>
+     
+     <EventoGrid/>
     </>
   )
 }
